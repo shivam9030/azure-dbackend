b9a6f1fe14c6d8c2f0996de349c32404b4a35d4c
shivam9030 <rajputshivam903@gmail.com>
2025-06-10 16:29:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-  origin: 'https://frontendfoodapp-hzh4dbgcd8dud4bm.canadacentral-01.azurewebsites.net/',
+  origin: 'https://frontendfoodapp-hzh4dbgcd8dud4bm.canadacentral-01.azurewebsites.net',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
